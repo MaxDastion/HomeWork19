c4f2765171fbe77d504abe2bcd47d5321a8dc0d9
Студент <@3-8>
2023-12-12 19:21:09 +0300

    Добавьте файлы проекта.

---
+++ Project2/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main() {
+	vector <int> v = { 2, 2, 2, 2, 3, 3, 3 };
+	for (size_t i = 0; v.size() >= 0; i++)
+	{
+		if (v.size() == 0) {
+			cout << i;
+			return;
+		}
+		
+		if (v.size() == 1) {
+			cout << -1;
+			return;
+		}
+		if (v.size()%2 == 0 && v.size() != 0) {
+			v.pop_back();
+			v.pop_back();
+		}
+		if (v.size() >= 3) {
+			v.pop_back();
+			v.pop_back();
+			v.pop_back();
+		}
+		
+		
+
+	}
+}
